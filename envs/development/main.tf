3aac5c2eae85734082b17ed9f0ee48e8228d0210
Jason Bisson <jasonbisson@google.com>
2022-08-05 11:58:29 -0400

    Updated network version to 4

--- envs/development/main.tf
+++ envs/development/main.tf
@@ -140,7 +140,7 @@ resource "google_compute_firewall" "internal" {
 
 module "gcp-network" {
   source       = "terraform-google-modules/network/google"
-  version      = "~> 3.1"
+  version = "~> 4.0"
   project_id   = var.project_id
   network_name = var.network
 
